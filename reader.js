b5a66f25472e443b47aaee41d2e737999bf15ca3
nandemotoken <nandemotoken@gmail.com>
2019-09-07 20:55:28 +0900

    first MVP

---
+++ reader.js
@@ -0,0 +1,60 @@
+//環境設定
+var Address = "0x24e7e143a8474300b2ff599ebcc4044c89b15c49";
+
+//metamaskの準備
+web3js = new Web3(web3.currentProvider);	
+//スマートコントラクト読み込み
+mycontract = new web3js.eth.Contract(ABI , Address);
+
+//1.00-beta.36と表示されるのが正しい
+console.log( Web3.version );
+
+//metamask login処理
+ethereum.enable().then(console.log);
+
+
+//---------------------read--------------------------
+//コントラクトのread処理は次の書式に統一します。
+//定義
+//function r_functionN( r_resultN ){ javascriptの処理内容...console.log(read_resultN)など }
+//
+//利用
+//mycontract.methods.getnumber().call().then(r_functionN);
+//...solidityの関数呼出の結果が、r_functionNで処理される。
+//---------------------------------------------------
+
+function r_function1( r_result1 ){ console.log( "ans1:" +  r_result1); }
+function r_function2( r_result2 ){ console.log( "ans2:" +  r_result2); }
+function r_function3( r_result3 ){ console.log( "ans3:" +  r_result3); }
+function r_function4( r_result4 ){ console.log( "ans4:" + r_result4); }
+
+function getAnswer1( hiki ){
+	mycontract.methods.getAnswer(1).call({from: hiki[0]}).then(r_function1);
+}
+
+
+function getAnswer2( hiki ){
+	mycontract.methods.getAnswer(2).call({from: hiki[0]}).then(r_function2);
+}
+
+
+function getAnswer3( hiki ){
+	mycontract.methods.getAnswer(3).call({from: hiki[0]}).then(r_function3);
+}
+
+
+function getAnswer4( hiki ){
+	mycontract.methods.getAnswer(4).call({from: hiki[0]}).then(r_function4);
+}
+
+//addr = web3js.eth.getAccounts().then(console.log);
+
+web3js.eth.getAccounts().then( getAnswer1 );
+web3js.eth.getAccounts().then( getAnswer2 );
+web3js.eth.getAccounts().then( getAnswer3 );
+web3js.eth.getAccounts().then( getAnswer4 );
+
+
+//mycontract.methods.getTrue().call().then(r_function1);
+//addr = web3js.eth.getAccounts().then(console.log);
+//mycontract.methods.getAnswer(1).call({from: '0xE35B827177398D8d2FBA304d9cF53bc8fC1573B7' }).then(r_function1);
---
+++ writer.js
@@ -0,0 +1,49 @@
+//環境設定
+var Address = "0x24e7e143a8474300b2ff599ebcc4044c89b15c49";
+
+//metamaskの準備
+web3js = new Web3(web3.currentProvider);	
+//スマートコントラクト読み込み
+mycontract = new web3js.eth.Contract(ABI , Address);
+
+//1.00-beta.36と表示されるのが正しい
+console.log( Web3.version );
+
+
+//---------------------write-------------------------
+//コントラクトのwrite処理は次の書式に統一します。
+//定義
+//function w_functionN( hidari , migi ){ mycontract.methods.setnumber(setnum).send({from: migi[0] });}
+//
+//利用
+//web3js.eth.getAccounts( w_functionN );
+//...metamaskの準備が出来たらw_functionNを実行する
+//--------------------------------------------------
+
+
+//var tmp_info;
+//function w_function1( hidari , migi ){ mycontract.methods.setnumber(tmp_info).send({from: migi[0] });}
+//function simple_test(){ web3js.eth.getAccounts( w_function1 ); }
+//function w_f1( t ){ 
+//	tmp_info = Number(t);
+//	simple_test();
+//}
+
+
+function sendanswer1true( hidari , migi ){ mycontract.methods.setAnswer( 1, true ).send({from: migi[0]}); }
+function sendanswer1false( hidari , migi ){ mycontract.methods.setAnswer( 1, false ).send({from: migi[0]}); }
+function sendanswer2true( hidari , migi ){ mycontract.methods.setAnswer( 2, true ).send({from: migi[0]}); }
+function sendanswer2false( hidari , migi ){ mycontract.methods.setAnswer( 2, false ).send({from: migi[0]}); }
+function sendanswer3true( hidari , migi ){ mycontract.methods.setAnswer( 3, true ).send({from: migi[0]}); }
+function sendanswer3false( hidari , migi ){ mycontract.methods.setAnswer( 3, false ).send({from: migi[0]}); }
+function sendanswer4true( hidari , migi ){ mycontract.methods.setAnswer( 4, true ).send({from: migi[0]}); }
+function sendanswer4false( hidari , migi ){ mycontract.methods.setAnswer( 4, false ).send({from: migi[0]}); }
+
+function sendanswer( hidari , migi ){ mycontract.methods.challangeCertification( ).send({from: migi[0]}); }
+
+
+
+//web3js.eth.getAccounts( sendanswer1 );
+
+
+
